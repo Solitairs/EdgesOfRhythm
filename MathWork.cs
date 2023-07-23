cefb0c0cab1d819f0779d68980a1d9d305ba1f45
Solitairs <108802036+Solitairs@users.noreply.github.com>
2023-07-23 15:31:21 +0800

    Create MathWork.cs
    try to be ready for work

---
+++ MathWork.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public MathWork : MonoBehaviour{
+  public struct NotePos{
+    float distance=1000;
+    int road=1;
+  };
+  public NotePos GetIPos(float NTime, float DTime, float ISpeed, int Iroad, int Droad){
+    
+  }
+}
