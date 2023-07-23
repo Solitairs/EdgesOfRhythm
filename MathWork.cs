06b18f28dc173fb53b14d9ade74c24b74ca955c2
Solitairs <108802036+Solitairs@users.noreply.github.com>
2023-07-23 15:35:55 +0800

    Update MathWork.cs
    try to be ready for work

--- MathWork.cs
+++ MathWork.cs
@@ -5,8 +5,8 @@ public MathWork : MonoBehaviour{
   public struct NotePos{
     float distance=1000;
     int road=1;
-  };
+  };//Save every note's real time position information
   public NotePos GetIPos(float NTime, float DTime, float ISpeed, int Iroad, int Droad){
     
-  }
+  }//Instantiate
 }
